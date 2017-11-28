ad37b6d9543488d0f453133edca9ea661593a4a4
Grzegorz Laskowski <grzegorz.laskowski@getresponse.com>
2017-11-28 22:23:37 +0100

    conf

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
 monsterfication
 ===============
 
-A Symfony project created on November 27, 2017, 2:03 pm.
+#INSTALL
+### PROJECT
+* git clone https://github.com/loigez/monsterfication.git
+* cd monsterfication
+* composer install
+###SERVER
+* cd monsterfication
+./bin/php bin/console server:run
+###DB
+* docker pull mariadb:latest
+* docker run -p 8306:3306 -e MYSQL_ROOT_PASSWORD=root -d mariadb
+* php bin/console doctrine:database:create
+
+
+##TIPS
+* generate schema
+php bin/console doctrine:schema:update --force
+
+* validate schema
+php bin/console doctrine:schema:validate
---
+++ src/AppBundle/Entity/Monsters.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="monsters")
+ */
+class Monsters
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param mixed $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $name;
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+}
