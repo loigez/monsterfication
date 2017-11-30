4baa063b68cc2bd9214fe07610817196cdaad1ee
Grzegorz Laskowski <grzegorz.laskowski@getresponse.com>
2017-11-30 20:09:49 +0100

    assigne badges after registrate

---
+++ src/AppBundle/DomainModel/UserBadgeProgressService.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace AppBundle\DomainModel;
+
+use AppBundle\Entity\Badge;
+use AppBundle\Entity\User;
+use AppBundle\Entity\UserBadgeProgress;
+use Doctrine\ORM\EntityManagerInterface;
+
+
+class UserBadgeProgressService
+{
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    /**
+     * BadgeService constructor.
+     * @param EntityManagerInterface $entityManager
+     */
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * @param User $user
+     * @param Badge[] $badges
+     */
+    public function assignBadgesToUser(User $user, array $badges)
+    {
+        foreach ($badges as $badge) {
+            $badgesToUser = new UserBadgeProgress();
+            $badgesToUser->setUser($user);
+            $badgesToUser->setState(0);
+            $badgesToUser->setProgress(0);
+            $badgesToUser->setBadge($badge);
+            $this->entityManager->persist($badgesToUser);
+        }
+
+        $this->entityManager->flush();
+    }
+
+
+
+
+}
