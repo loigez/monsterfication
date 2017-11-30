70848781f4819babeb9b742d4f1c9c811add88c6
Tomasz Galiński <tomasz.galinski@getresponse.com>
2017-11-30 23:10:38 +0100

    dodanie daty

--- src/AppBundle/Entity/Rules/BabyStepsRule.php
+++ src/AppBundle/Entity/Rules/BabyStepsRule.php
@@ -2,36 +2,19 @@
 
 namespace AppBundle\Entity\Rules;
 
-use AppBundle\DomainModel\Commit;
 use AppBundle\Entity\State;
 use AppBundle\Entity\UserBadgeProgress;
+use DateTime;
 
-class BabyStepsRule
+class BabyStepsRule extends Rule
 {
-    /**
-     * @var UserBadgeProgress
-     */
-    private $progressBadge;
-    /**
-     * @var Commit
-     */
-    private $commit;
-
-    /**
-     * BabyStepsRule constructor.
-     * @param UserBadgeProgress $progressBadge
-     * @param Commit $commit
-     */
-    public function __construct(UserBadgeProgress $progressBadge, Commit $commit)
-    {
-        $this->progressBadge = $progressBadge;
-        $this->commit = $commit;
-    }
 
     public function updateProgress(): UserBadgeProgress
     {
         $this->progressBadge->setState(State::UNLOCKED);
+        $this->progressBadge->setUnlockDate(new DateTime());
         $this->progressBadge->setProgress($this->progressBadge->getBadge()->getTarget());
+
         return $this->progressBadge;
     }
 
