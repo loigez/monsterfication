fd57643385168e07836991fc9787d6ab2e62cde3
Tomasz Galiński <tomasz.galinski@getresponse.com>
2017-12-01 03:31:03 +0100

    new rules

---
+++ src/AppBundle/Entity/Rules/MakeCommitsNumberInGrEchoRule.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace AppBundle\Entity\Rules;
+
+class MakeCommitsNumberInGrEchoRule extends MakeCommitsNumberInProjectRule
+{
+    const PROJECT_CODE_NAME = 'GRECHO';
+}
---
+++ src/AppBundle/Entity/Rules/MakeCommitsNumberInGrSierraRule.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace AppBundle\Entity\Rules;
+
+class MakeCommitsNumberInGrSierraRule extends MakeCommitsNumberInProjectRule
+{
+    const PROJECT_CODE_NAME = 'GRSIERRA';
+}
